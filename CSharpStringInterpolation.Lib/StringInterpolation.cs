6b7e8b5c5b214930dd53a80bc4f7d1e173d7f4af
Karthik Ananthapadmanaban <karthika25@gmail.com>
2014-05-12 17:42:50 -0400

    update stringinterpolation class to use the new helpers

--- CSharpStringInterpolation.Lib/StringInterpolation.cs
+++ CSharpStringInterpolation.Lib/StringInterpolation.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text.RegularExpressions;
 
 namespace CSharpStringInterpolation.Lib
 {
@@ -24,31 +20,16 @@ namespace CSharpStringInterpolation.Lib
                where T : class
         {
             var constructedString = str;
-
-            var matches = Interpolatables.Matches(str);
-            var varList = (from Match match in matches
-                           select PropertExtractor.Match(match.Value).Groups[3].Value).ToList();
-
-            var propertyInfos = t.GetType().GetProperties();
-            foreach (var prop in varList)
-            {
-                var pValue = GetPropertyValue(t, propertyInfos, prop);
-                constructedString = constructedString.Replace(GetReplaceStrFunc(prop), pValue);
-            }
+            
+            var interpolatables = t.InterpolatablesOf(str);
+            interpolatables.ForEach(interpolatable =>
+                {
+                    constructedString = constructedString.Replace(GetReplaceStrFunc(interpolatable.Item), interpolatable.Value);    
+                });
 
             return constructedString;
         }
 
-        private static string GetPropertyValue<T>(T t, IEnumerable<PropertyInfo> propertyInfos, string s)
-                where T : class
-        {
-            var prop = propertyInfos.Single(p => p.Name == s);
-            var pValue = prop.GetValue(t, null);
-            return pValue as string != null ? (string)pValue : pValue.ToString();
-        }
-
-        private static readonly Regex Interpolatables = new Regex(@"\#\{[a-zA-Z]+\}");
-        private static readonly Regex PropertExtractor = new Regex(@"(\#)(\{)([a-zA-Z]+)(\})");
         private static readonly Func<string, string> GetReplaceStrFunc = prop => string.Format(@"#{{{0}}}", prop);
     }
 }
